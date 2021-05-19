4e365f98a83346818cdb590b9065cae8f98f5947
Oliver Mills <mills.oliver@ymail.com>
2021-05-18 20:23:26 -0400

    Character walks how I want him too now
    added an integer to choose an idle direction

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -20,6 +20,27 @@ public class CharacterController : MonoBehaviour
         playerAnimator.SetFloat("Horizontal", movement.x);
         playerAnimator.SetFloat("Vertical", movement.y);
         playerAnimator.SetFloat("Speed", movement.sqrMagnitude);
+        // if moving right
+        if(movement.x > 0)
+        {
+            playerAnimator.SetInteger("Direction",1);
+        }
+        // if moving left
+        else if(movement.x < 0)
+        {
+            playerAnimator.SetInteger("Direction",3);
+        }
+        // if moving up
+        else if(movement.y > 0)
+        {
+            playerAnimator.SetInteger("Direction",0);
+        }
+        // if moving down
+        else if(movement.y < 0)
+        {
+            playerAnimator.SetInteger("Direction",2);
+        }
+        Debug.Log(movement);
     }
     /// <summary>
     /// Used to execute the input
